6f0c8c40d4aa080cc885b754776c96f0180466e6
Leyjaj <leyjajvr@gmail.com>
2025-12-20 14:13:57 -0700

    solucion imagenes que no aparecen xdxd

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,45 @@
 import type { NextConfig } from "next";
 import withPlaiceholder from "@plaiceholder/next";
 
-const nextConfig: NextConfig = {  
-  experimental:{
-    serverActions:{      
-      allowedOrigins:["*","127.0.0.1:8787", "localhost:3000","aquafornais.dpdns.org","*.dpdns.org","8787-firebase-aquafornite-1747146266938.cluster-iesosxm5fzdewqvhlwn5qivgry.cloudworkstations.dev/"]
-    }
-  },
-  images:{        
-    remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'media.fortniteapi.io',              
-        },
-        {
-         protocol:"https",
-         hostname:"fortnite-api.com" 
-        },
-        {
-          protocol:"https",
-          hostname:"cdn.fortnite-api.com" 
-         },
-        {
-          protocol: 'https',
-          hostname: 'placehold.co',              
-        },
-        {
-          protocol: 'https',
-          hostname: 'fakeimg.pl',
-        },
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "*",
+        "127.0.0.1:8787",
+        "localhost:3000",
+        "aquafornais.dpdns.org",
+        "*.dpdns.org",
+        "8787-firebase-aquafornite-1747146266938.cluster-iesosxm5fzdewqvhlwn5qivgry.cloudworkstations.dev/",
       ],
-}
+    },
+  },
+
+  images: {
+    unoptimized: true, // ✅ DESACTIVA EL IMAGE OPTIMIZER DE VERCEL (SOLUCIÓN AL 402)
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.fortniteapi.io",
+      },
+      {
+        protocol: "https",
+        hostname: "fortnite-api.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.fortnite-api.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "fakeimg.pl",
+      },
+    ],
+  },
 };
 
 export default withPlaiceholder(nextConfig);
