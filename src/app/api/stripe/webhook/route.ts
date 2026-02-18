7d3f575455119a734c28a300454ad10862b37cdb
Leyjaj <leyjajvr@gmail.com>
2026-02-17 21:33:43 -0700

    Actualizar route.ts

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -3,9 +3,7 @@ export const runtime = "nodejs";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16", // Asegúrate de usar una versión compatible
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(req: Request) {
   const body = await req.text();
