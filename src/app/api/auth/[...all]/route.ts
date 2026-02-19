2c336617e30f3cff48e0e6b64d1791310c66804e
Leyjaj <leyjajvr@gmail.com>
2026-02-19 16:51:15 -0700

    Actualizar route.ts

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,6 @@
 import { auth } from "@/lib/auth";
-import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = toNextJsHandler(auth);
+export const runtime = "edge";
+
+export const GET = auth.handler;
+export const POST = auth.handler;
