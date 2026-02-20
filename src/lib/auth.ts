e7c83a0962fc470731446d907c5de37757c62c3b
Leyjaj <leyjajvr@gmail.com>
2026-02-19 18:50:01 -0700

    Actualizar auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,19 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import { nextCookies } from "better-auth/next-js";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -29,7 +41,6 @@ export const auth = betterAuth({
     discord: {
       clientId: process.env.DISCORD_CLIENT_ID as string,
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
-      permissions: 2048 | 16384,
       mapProfileToUser: async (profile) => {
         return {
           discordId: profile.id,
