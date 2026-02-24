641449905e21b20be66e2332ff15e933a15a5c0c
Leyjaj <leyjajvr@gmail.com>
2026-02-23 19:27:41 -0700

    Actualizar auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,10 @@
+"use client";
+
 import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL!,
-    disableCSRFCheck:process.env.NODE_ENV ==="development"
-     
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL!,
+  disableCSRFCheck: process.env.NODE_ENV === "development",
 });
+
 export const { signIn, signOut, useSession } = authClient;
